b0969677eff4fdf62145a2e9f0f9967d736e6acf
gigabate3@gmail.com <toli1997M>
2021-03-16 19:19:17 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import TableData from "./tableData/tableData";
+import Header from "./header/header";
+import ControlData from "./controlData/controlData";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+              <Header />
+              <ControlData />
+              <TableData />
       </header>
     </div>
   );

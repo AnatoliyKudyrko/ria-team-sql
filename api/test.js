a41ddffb70e8a4d044b85b1c4687c4ca50cf3e36
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2021-03-20 15:34:10 +0200

    api3

--- api/test.js
+++ api/test.js
@@ -2,6 +2,9 @@ const io = require("socket.io-client");
 
 const SERVER = "http://127.0.0.1:4000";
         const socket = io(SERVER);
+        socket.emit("getTables", (err, res) => {
+            console.log(res);
+        });
         socket.emit("getFields", 'slon', 'facts', (err, res) => {
             console.log(res);
         });

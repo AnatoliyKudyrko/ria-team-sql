f97a2536b55b9e10de367593020c34057da59d41
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2021-03-26 20:07:08 +0200

    users2

--- api/managers/mysql.js
+++ api/managers/mysql.js
@@ -1,14 +1,25 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const config = require('../config').mysql;
+let db = (function(){
+    let instance;
+    return {
+        connection: async function(){
+            if (instance == null) {
+                instance = await mysql.createConnection(config);
+            }
+            return instance;
+        }
+   };
+})();
 
-
-const connection = mysql.createConnection(config);
 
 async function createUser({login, password, first_name, last_name}) {
+
     try {
+        const connection = await db.connection();
         const [[isExists]] = await connection.execute(`SELECT login FROM users WHERE login = '${login}'`);
         if (!isExists) {
-            [rows] = await connection.execute(`INSERT INTO users (login, password, first_name, last_name) VALUES ('${login}', '${password}', '${first_name}', '${last_name}')`);
+           const [rows] = await connection.execute(`INSERT INTO users (login, password, first_name, last_name) VALUES ('${login}', '${password}', '${first_name}', '${last_name}')`);
         } else {
             throw new Error(`${login} is already present`);
         }
@@ -21,7 +32,7 @@ async function createUser({login, password, first_name, last_name}) {
 
 async function updateUser({user_id, login, password, first_name, last_name}) {
     try {
-
+        const connection = await db.connection();
         const [rows, fields] = await connection.execute(`UPDATE users SET  login = '${login}', password = '${password}', first_name = '${first_name}', last_name = '${last_name}') 
         WHERE user_id = '${user_id}'`);
         
@@ -34,22 +45,34 @@ async function updateUser({user_id, login, password, first_name, last_name}) {
 
 async function checkUser({login, password}) {
     try {
-        const [[id]] = await connection.execute(`SELECT user_id FROM users WHERE login = '${login}' AND password = '${password}'`);
-        console.log(id);
-        if (!id) {
-            const [[logId]] = await connection.execute(`SELECT user_id FROM users WHERE login = '${login}'`);
-            const err = logId ? 'Password is wrong' : 'Login isn\'t registered';
+        const connection = await db.connection();
+        const user_id = await connection.execute(`SELECT user_id FROM users WHERE login = '${login}' AND password = '${password}'`);
+        console.log(user_id);
+        if (!user_id) {
+            const {userIsRegistered} = await checkLogin({login});
+            const err = userIsRegistered ? 'Password is wrong' : 'Login isn\'t registered';
             throw new Error (err);
         }
-        return {id};
+        return {user_id};
     } catch (error) {
         console.error(error);
         return error;
     };
 }
 
+async function checkLogin({login}) {
+    try {
+        const connection = await db.connection();
+        const [rows] = await connection.execute(`SELECT user_id FROM users WHERE login = '${login}'`);
+        return {userRegisteredId: rows.length ? row[0].user_id : false}
+    } catch (error) {
+        return {error};
+    };
+};
+
 async function deleteUser({user_id}) {
     try {
+        const connection = await db.connection();
         await connection.execute(`DELETE user_id FROM users WHERE user_id = '${user_id}'`);
         return {id};
     } catch (error) {
@@ -58,4 +81,4 @@ async function deleteUser({user_id}) {
     };
 }
 
-module.exports = {createUser, checkUser, updateUser, deleteUser};
+module.exports = {createUser, checkUser, updateUser, deleteUser, checkLogin};

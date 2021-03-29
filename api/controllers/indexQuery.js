725a6650404be0ff62e2c30442d8bc1b3a4d9c49
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2021-03-29 11:14:25 +0300

    fix1

--- api/controllers/indexQuery.js
+++ api/controllers/indexQuery.js
@@ -1,16 +1,16 @@
 const myDb = require('../managers/mysql');
 
 async function createQuery(data, cb) {
-    return cb(await myDb.createQuery({ ...data }));
+    return cb(null, await myDb.createQuery({ ...data }));
 }
 
 
 async function updateQuery(data, cb) {
-    return cb(await myDb.updateQuery({ ...data }));
+    return cb(null, await myDb.updateQuery({ ...data }));
 }
 
 
 async function selectQueries(data, cb) {
-    return cb(await myDb.selectQueries({ ...data }));
+    return cb(null, await myDb.selectQueries({ ...data }));
 }
 module.exports = { createQuery, updateQuery, selectQueries }
